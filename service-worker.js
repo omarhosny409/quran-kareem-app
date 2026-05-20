e89e58419820477223c77b67b77b235844e368ac
Omar Hosny <omarhosny10100@gmail.com>
2026-05-20 17:02:46 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'quran-kareem-mobile-v2';
+const CACHE_VERSION = 'quran-kareem-mobile-v3';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
