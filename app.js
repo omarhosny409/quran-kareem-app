f195b47918bd1cf20a92060b792964da52c9ab56
Omar Hosny <omarhosny10100@gmail.com>
2026-05-21 13:59:17 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 (function () {
   'use strict';
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-      navigator.serviceWorker.register('./service-worker.js', { scope: './' }).catch(function () {});
-    });
-  }
+  if (!('serviceWorker' in navigator)) return;
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('./service-worker.js', { scope: './' }).then(function (reg) {
+      try { reg.update(); } catch (e) {}
+      if (reg.waiting) reg.waiting.postMessage({ type: 'SKIP_WAITING' });
+    }).catch(function () {});
+  });
 })();
